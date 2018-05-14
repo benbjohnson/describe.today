9906eda9fba61eb2c6afb6d0a8eb1c6b704e7424
Ben Johnson <benbjohnson@yahoo.com>
2018-05-14 08:49:16 -0600

    Randomize words.

---
+++ words/th.js
@@ -0,0 +1,18 @@
+words.th = [
+"thematic",
+"thankful",
+"throwback",
+"therapeutic",
+"thrifty",
+"thoughtful",
+"thirsty",
+"theoretical",
+"thievish",
+"thunderstruck",
+"thriving",
+"thwarting",
+"theatrical",
+"thinking",
+"thespian",
+"thrilling",
+];
